1a94d0785b866cd336e24ef9b7968eb95e34350d
Florian Albertz <florian@albertz.io>
2016-06-11 13:05:03 +0200

    Pretty up the debug output

--- src/print.js
+++ src/print.js
@@ -4,10 +4,11 @@ export default function prettyPrint(record) {
 	let padding = new Array(Math.max(5 - record.level.length, 0) + 1).join(' ');
 
 	let time = (new Date(record.time)).toLocaleString();
-	let path = record.scope.length > 0 ? record.scope.join(chalk.dim(' > ')) + ' > ' : '';
+	let path = record.scope.length > 0 ? record.scope.join(chalk.dim(' > ')) + (record.message ? ' > ' : '') : '';
 	let level = record.level + padding;
 	let levelColor = { trace: 'white', info: 'green', warn: 'yellow', error: 'red' }[record.level] || 'gray';
-	let message = (typeof record.data === 'string') ? record.data : JSON.stringify(record.data);
+	let message = record.message || '';
+	let data = record.data ? `[ ${chalk.dim(JSON.stringify(record.data))} ]` : '';
 
-	return `[ ${chalk.blue(time)} ][ ${chalk[levelColor](level)} ] ${chalk.gray(path)}${message}`;
+	return `[ ${chalk.blue(time)} ][ ${chalk[levelColor](level)} ]${data} ${chalk.gray(path)}${chalk.white(message)}`;
 }
